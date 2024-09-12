6e107fa595eeffc8c09535b572136ddb702970c7
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-09-12 16:52:00 -0700

    Time: 486 ms (5.02%), Space: 18.9 MB (80.96%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,6 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        
+        n=  len(nums)
+        ans = [0] * n
+        ans[0] = 1
+        
+        for i in range(1, n):
+            ans[i] = ans[i - 1] * nums[i - 1]
+        
+        R = 1
+        
+        for i in range(n - 1, -1, -1):
+            ans[i] *= R
+            R *= nums[i]
+            
+        
+        return ans
+        
+        
+        
+        
+        
         n = len(nums)
         left = [1] * n
         right= [1] * n
