2729f7c6cd2105b5b74366c88168fcc193e46e32
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-09-11 18:27:58 -0700

    Time: 35 ms (96.55%), Space: 16.4 MB (88.82%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -4,27 +4,11 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        if not s:
-            return 0
-        charMap = {
-            "I" : 1,
-            "V" : 5,
-            "X" : 10,
-            "L" : 50,
-            "C" : 100,
-            "D" : 500,
-            "M" : 1000   
-        }
-        n = len(s)
-        tot = 0
-        for i in range(n - 1):        
-            if s[i] == "I" and s[i + 1] in ("V", "X"):
-                tot -= 1                
-            elif s[i] == "X" and s[i + 1] in ("L", "C"):
-                tot -= 10                
-            elif s[i] == "C" and s[i + 1] in ("D", "M"):
-                tot -= 100
+        hash1={"I":1,"V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        res=0
+        for i in range(len(s)):
+            if i+1<len(s) and hash1[s[i]]<hash1[s[i+1]]:
+                res-=hash1[s[i]]
             else:
-                tot += charMap[s[i]]
-        tot += charMap[s[n - 1]]
-        return tot    
+                res+=hash1[s[i]]
+        return res
