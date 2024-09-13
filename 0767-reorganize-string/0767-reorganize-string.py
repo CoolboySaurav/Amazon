193175879a0d782f12be62db65f90b6baa6e5efd
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-09-13 15:54:46 -0700

    Time: 15 ms (74.52%), Space: 11.5 MB (93.29%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -7,6 +7,41 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        # O(N) unique algo 
+        charCount = Counter(s)
+        maxCount, letter = 0, ""
+        
+        for c, cnt in charCount.items():
+            if cnt > maxCount:
+                maxCount = cnt
+                letter = c
+        
+        if maxCount > (len(s) + 1) // 2:
+            return ""
+        
+        ans = [""] * len(s)
+        index = 0
+        
+        while charCount[letter] > 0:
+            ans[index] = letter
+            index += 2
+            charCount[letter] -= 1
+        
+        
+        for char, count in charCount.items():
+            while count > 0:
+                if index >= len(s):
+                    index = 1
+                    
+                ans[index] = char
+                index += 2
+                count -= 1
+        return "".join(ans)
+        
+   
+        
+        # Neetcode easy to understand priority queue solution with tracking previous char
+        
         maxHeap = [[- cnt, c] for c, cnt in Counter(s).items()]
         heapq.heapify(maxHeap)
         
